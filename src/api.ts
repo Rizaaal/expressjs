f671e79a164bcf42294b97ffaf6755200dda9968
Rizal <gabrieledigrazia@protonmail.com>
2025-12-04 16:46:12 +0100

    feature: add get items

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+const { getItems } = require('./db');
 
 export const app = express();
 
@@ -10,8 +11,13 @@ app.use(express.raw({ type: 'application/vnd.custom-type' }));
 app.use(express.text({ type: 'text/html' }));
 
 // Healthcheck endpoint
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   res.status(200).send({ status: 'ok' });
+});
+
+app.get('/items', async (req, res) => {
+  const items = await getItems();
+  res.json(items);
 });
 
 const api = express.Router();
